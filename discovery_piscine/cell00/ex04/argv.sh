b4dac2134bced390ae7aa4d85c6d5477a4564ec1
hon <honhonhonx312@gmai.com>
2023-06-02 14:46:52 +0700

    Added new feature

---
+++ discovery_piscine/cell00/ex04/argv.sh
@@ -0,0 +1,18 @@
+if [ $# -lt 1 ]; then
+  echo "No arguments supplied"
+  exit 0
+fi
+
+echo "$1"
+
+if [ $# -lt 2 ]; then
+  exit 0
+fi
+
+echo "$2"
+
+if [ $# -lt 3 ]; then
+  exit 0
+fi
+
+echo "$3"
